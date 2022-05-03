6b663a71350397d2f255ae8ece53d9dddd2b176f
Anna Zharova <a.zharova.muctr@gmail.com>
2022-05-03 13:53:12 +0300

    executable files without code

---
+++ src/even.js
@@ -0,0 +1,31 @@
+import readlineSync from 'readline-sync';
+import randomInteger from './randomnumber.js';
+
+const ParityCheckGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const UserName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${UserName}!`);
+  console.log('Answer "yes" if the number is even, other answer "no"');
+  let counterCorrectAnswer = 0;
+  while (counterCorrectAnswer < 3) {
+    const RandomNum = randomInteger(0, 100);
+    let CorrectAnswer = '';
+    if (RandomNum % 2 !== 0) {
+      CorrectAnswer = 'no';
+    } else {
+      CorrectAnswer = 'yes';
+    }
+    const result = CorrectAnswer;
+    console.log(`Question: ${RandomNum}`);
+    const UserAnswer = readlineSync.question('You answer: ');
+    if (UserAnswer === result) {
+      console.log('Correct!');
+      counterCorrectAnswer += 1;
+    } else {
+      console.log(`${UserAnswer} is wrong answer ;(. Correct answer was ${result}\nLet's try again, ${UserName}!`);
+      counterCorrectAnswer = 0;
+    }
+  }
+  return `Congratulations, ${UserName}!`;
+};
+export default ParityCheckGame;
---
+++ src/randomnumber.js
@@ -0,0 +1,5 @@
+function randomInteger(min, max) {
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+export default randomInteger;
