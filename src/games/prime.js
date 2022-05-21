efed6f60bb5f0c4bdb4b0504dbea7a28f56dc737
Anna Zharova <a.zharova.muctr@gmail.com>
2022-05-21 15:19:47 +0300

    prime game (2-100)

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,7 +3,7 @@ import gameLogic from '../index.js';
 
 const isPrime = (num) => {
   for (let i = 2; i < Math.sqrt(num); i += 1) {
-    if (num % i === 0 || num < 2) {
+    if (num % i === 0) {
       return false;
     }
   }
@@ -12,7 +12,7 @@ const isPrime = (num) => {
 
 const conditions = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const gamePrime = () => {
-  const randomNum = randomInteger(1, 100);
+  const randomNum = randomInteger(2, 100);
   const question = `${randomNum}`;
   const correctAnswer = isPrime(randomNum) ? 'yes' : 'no';
   return [question, correctAnswer];
